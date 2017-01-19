91f30b8ab3a38fd4fad31045543914f5f4bc03b3
Liz Bayardelle <lizbayardelle@gmail.com>
2017-01-19 07:03:24 -0800

    Footer

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users, :controllers => { registrations: 'registrations' }
+
   get 'home/index'
   root 'home#index'
+  
   get 'home/info'
   get 'home/export'
   
